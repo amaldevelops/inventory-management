b8f5a49580877427f39c2ed972c8f872548b608b
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-12-27 20:07:53 +1100

    Added Form for admin view

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,16 +1,14 @@
 const db = require("../db/queries");
 
 async function getAllInventoryItems(req, res) {
-  const allProducts= await db.getPopulateAllProducts();
+  const allProducts = await db.getPopulateAllProducts();
   console.log(allProducts);
-  res.render("index", {allProducts:allProducts});
+  res.render("index", { allProducts: allProducts });
 }
 
 async function adminDashboard(req, res) {
- 
   res.render("storeAdminDashboard");
 }
-
 
 module.exports = {
   getAllInventoryItems,
