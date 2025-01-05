01d0e2a6dbc7d596a0b6dcfedfdbc9b76a3b9cf9
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-01-06 09:08:38 +1100

    Changed pool.js settings to use .env file

--- db/pool.js
+++ db/pool.js
@@ -1,14 +1,13 @@
 #! /usr/bin/env node
 
-const {Pool}=require("pg");
+const { Pool } = require("pg");
 
 require("dotenv").config();
 
-module.exports=new Pool({
-    host:process.env.HOST,
-    user:process.env.USER,
-    database:"inventory",
-    password:process.env.INVENTORY_DATABASE_PASSWORD,
-    port:5432
-
+module.exports = new Pool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DATABASE_NAME,
+  password: process.env.INVENTORY_DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
 });
